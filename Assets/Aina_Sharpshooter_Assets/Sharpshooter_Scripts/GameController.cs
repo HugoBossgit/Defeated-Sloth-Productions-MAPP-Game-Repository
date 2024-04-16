e86d2ca80ad2191af94bc7b5bb59f8555ce853ef
Rosehip-Soup <cooling.aina@gmail.com>
2024-04-16 21:03:19 +0200

    Fixade viktiga mekaniker
    Fixade en del i editorn och lade till en del gameobjects, skrev även på scripten och man kan nu skjuta och eliminera fiender.
    Det finns dock ingen Game Over eller Win

---
+++ Assets/Aina_Sharpshooter_Assets/Sharpshooter_Scripts/GameController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private int points;
+    [SerializeField] private float multiplier = 1.0f;
+    private int comboStarter;
+    [SerializeField] private int lives = 3;
+
+    [SerializeField] private TMP_Text pointsText;
+    [SerializeField] private TMP_Text livesText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addPoints(int value)
+    {
+        points += (int)(value * multiplier);
+    }
+
+    public void combo(Boolean increase)
+    {
+        comboStarter++;
+        if (increase && comboStarter > 2) 
+        {
+            multiplier += 0.1f;
+        }
+        
+        if(!increase)
+        {
+            multiplier = 1.0f;
+        }
+    }
+
+    public void DecrementLives(int decrease)
+    {
+        lives -= decrease;
+    }
+
+    public void Loss()
+    { }
+
+    public void Win()
+    { }
+
+    public void Restart()
+    { }
+
+}
