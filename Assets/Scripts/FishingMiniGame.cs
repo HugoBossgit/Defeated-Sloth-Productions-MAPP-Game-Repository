02b956cd146acc5b1e1510a89cd51b36730e8c24
Kajsaherge <kajsaherge@gmail.com>
2024-04-18 12:31:00 +0200

    Fishing game funktioner nästan färdiga
    nu är fishing mini game i princip klart. Minus grafik/ljud och en färdig game over menu. Just nu kommer du bara till en meny med en knapp tillbaka till spelet.

--- Assets/Scripts/FishingMiniGame.cs
+++ Assets/Scripts/FishingMiniGame.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FishingMiniGame : MonoBehaviour
 {
 
+    public GameObject GameOverMenu;
+
     [SerializeField] Transform topPivot;
     [SerializeField] Transform bottomPivot;
 
@@ -13,17 +15,80 @@ public class FishingMiniGame : MonoBehaviour
     float fishPosition;
     float fishDestination;
 
+    //bestämmer hur lång tid den är på en viss plats;
     float fishTimer;
-
     [SerializeField] float timerMultiplicator;
 
+    [SerializeField] float failTimer = 10f;
+
+    //hur snabbt den rör sig mellan punkter
     float fishSpeed;
     [SerializeField] float smoothMotion = 1f;
 
+    [SerializeField] Transform progressBarContainer;
+
+    [SerializeField] Transform hook;
+
+    [SerializeField] float hookSize = 0.1f;
+    [SerializeField] float hookPower = 5f;
+    [SerializeField] float hookPullPower = 0.01f;
+    [SerializeField] float hookGravity = 0.005f;
+    [SerializeField] float hookProgressDegradationPower = 1f;
+
+    float hookPosition;
+    float hookProgress;
+    float hookPullVelocity;
+
+    bool pause = false;
+
     private void Update()
     {
+        if (pause)
+        {
+            return;
+        }
+        Fish();
+        Hook();
+        ProgressCheck();
+    }
+
+    private void ProgressCheck()
+    {
+        Vector3 ls = progressBarContainer.localScale;
+        ls.y = hookProgress;
+        progressBarContainer.localScale = ls;
+
+        float min = hookPosition - hookSize / 2;
+        float max = hookPosition + hookSize / 2;
+
+        if(min < fishPosition && fishPosition < max)
+        {
+            hookProgress += hookPower * Time.deltaTime;
+        }
+        else
+        {
+            //om fisken inte är i hook så kommer hookpower långsamt gå ner
+            hookProgress -= hookProgressDegradationPower * Time.deltaTime;
+
+            failTimer -= Time.deltaTime;
+            if(failTimer < 0.5f)
+            {
+                Lose();
+            }
+        }
+
+        if (hookProgress >= 1f)
+        {
+            Win();
+        }
+
+        hookProgress = Mathf.Clamp(hookProgress, 0f, 1f);
+    }
+
+    private void Fish()
+    {
         fishTimer -= Time.deltaTime;
-        if(fishTimer < 0f)
+        if (fishTimer < 0f)
         {
             fishTimer = Random.value * timerMultiplicator;
 
@@ -32,5 +97,41 @@ public class FishingMiniGame : MonoBehaviour
 
         fishPosition = Mathf.SmoothDamp(fishPosition, fishDestination, ref fishSpeed, smoothMotion);
         fish.position = Vector3.Lerp(bottomPivot.position, topPivot.position, fishPosition);
+    }
+
+    private void Hook()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            hookPullVelocity += hookPullPower * Time.deltaTime;
+        }
+        hookPullVelocity -= hookGravity * Time.deltaTime;
+
+        hookPosition += hookPullVelocity;
+
+        if(hookPosition - hookSize / 2 <= 0f && hookPullVelocity < 0f)
+        {
+            hookPullVelocity = 0f;
+        }
+
+        if(hookPosition + hookSize / 2 >= 1f && hookPullVelocity > 0f)
+        {
+            hookPullVelocity = 0f;
+        }
+
+        hookPosition = Mathf.Clamp(hookPosition, hookSize / 2, 1 - hookSize / 2);
+        hook.position = Vector3.Lerp(bottomPivot.position, topPivot.position, hookPosition);
+    }
+
+    private void Win()
+    {
+        pause = true;
+        GameOverMenu.SetActive(true);
+    }
+
+    private void Lose()
+    {
+        pause = true;
+        GameOverMenu.SetActive(true);
     }
 }
