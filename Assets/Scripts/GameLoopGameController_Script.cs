83592313cc2496593e8a3ff8266ba8d646b4c072
HugoBossgit <hugo.lundberg@onlinepost.se>
2024-04-24 23:07:26 +0200

    Majoritet av game loop funktionalitet
    saknas boss, items, scenbyte vid encounter är ersatt med knappar i gameloop scenen

--- Assets/Scripts/GameLoopGameController_Script.cs
+++ Assets/Scripts/GameLoopGameController_Script.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using UnityEngine.LowLevel;
+using System.Xml;
 
 public class GameLoopGameController_Script : MonoBehaviour
 {
@@ -18,7 +20,14 @@ public class GameLoopGameController_Script : MonoBehaviour
     [SerializeField] private GameObject pauseButtonGameObject;
     [SerializeField] private GameObject gameWinPanelGameObject;
     [SerializeField] private GameObject gameLosePanelGameObject;
-    [SerializeField] private List<GameObject> enemies;
+    [SerializeField] private GameObject enemyOneGameObject;
+    [SerializeField] private GameObject enemyTwoGameObject;
+    [SerializeField] private GameObject eventOneGameObject;
+    [SerializeField] private GameObject eventTwoGameObject;
+    [SerializeField] private GameObject itemSwordGameObject;
+    [SerializeField] private GameObject itemShieldGameObject;
+    [SerializeField] private GameObject bossGameObject;
+
 
     private UnityEngine.UI.Slider playerProgressSlider, playerHealthSlider;
 
@@ -26,8 +35,12 @@ public class GameLoopGameController_Script : MonoBehaviour
 
     private bool gameIsPaused, gameIsWon, gameIsLost;
 
+    private int bossCurrentHealth;
+
+    private int bossMaxHealth = 50;
+
     //Antal steg till spel loopens slut i walking pace (1/1) vilket blir 60 sekunder
-    private int maxGameProgress = 60;
+    private int maxGameProgress = 40;
     public const int maxPlayerHealth = 10;
 
     private void Awake()
@@ -77,7 +90,7 @@ public class GameLoopGameController_Script : MonoBehaviour
             pauseButton = pauseButtonGameObject.GetComponent<UnityEngine.UI.Button>();
         }
 
-        InvokeRepeating(nameof(DoPlayerProgress), 1, 1);
+        InvokeRepeating(nameof(DoPlayerProgress), 0, 1);
     }
 
     private void DoPlayerProgress()
@@ -86,11 +99,11 @@ public class GameLoopGameController_Script : MonoBehaviour
         {
             if(Data.walking)
             {
-                Data.playerProgress += 1;
+                Data.playerProgress += 2;
             }
             if(Data.running)
             {
-                Data.playerProgress += 3;
+                Data.playerProgress += 4;
             }
 
             playerProgressSlider.value = Data.playerProgress;
@@ -117,19 +130,79 @@ public class GameLoopGameController_Script : MonoBehaviour
     {
         UpdateButtonColors();
         playerProgressSlider.value = Data.playerProgress;
+        playerHealthSlider.value = Data.playerHealth;
 
-        if (Data.playerProgress == maxGameProgress && !gameIsWon)
+        if (Data.playerProgress == maxGameProgress && !gameIsWon) //win on max progress
         {
             gameIsWon = true;
             WinGame();
             Debug.Log("win");
         }
 
-        if (Data.playerHealth <= 0 && !gameIsLost)
+        if (Data.playerHealth <= 0 && !gameIsLost) //lose when health is 0
         {
             gameIsLost = true;
             LoseGame();
             Debug.Log("Launching game in game loop scene results in health = 0 which is game lose");
+        }
+
+        if (Data.playerLose) //player loses encounter
+        {
+            if (Data.activeEventOrEnemy == "ENEMY")
+            {
+                Data.playerLose = false;
+                Data.activeEventOrEnemy = "";
+                PlayerLoseInEnemy();
+            }
+            else if(Data.activeEventOrEnemy == "EVENT")
+            {
+                Data.playerLose = false;
+                Data.activeEventOrEnemy = "";
+                PlayerLoseInEvent();
+            }
+
+        }
+
+        if (Data.playerWin) //player wins encounter
+        {
+            if (Data.activeEventOrEnemy == "ENEMY")
+            {
+                Data.playerWin = false;
+                Data.activeEventOrEnemy = "";
+                PlayerWinInEnemy();
+            }
+            else if (Data.activeEventOrEnemy == "EVENT")
+            {
+                Data.playerWin= false;
+                Data.activeEventOrEnemy = "";
+                PlayerWinInEvent();
+            }
+        }
+
+        //check progress
+
+        if (Data.playerProgress > 5 && !Data.encounterOneMet)
+        {
+            Data.encounterOneMet = true;
+            GetRandomEventOrEnemy();
+        }
+
+        if (Data.playerProgress > 15 && !Data.encounterTwoMet)
+        {
+            Data.encounterTwoMet = true;
+            GetRandomEventOrEnemy();
+        }
+
+        if (Data.playerProgress > 25 && !Data.encounterThreeMet)
+        {
+            Data.encounterThreeMet = true;
+            GetRandomEventOrEnemy();
+        }
+
+        if (Data.playerProgress > 35 && !Data.encounterFourMet)
+        {
+            Data.encounterFourMet = true;
+            GetRandomEventOrEnemy();
         }
     }
 
@@ -167,7 +240,6 @@ public class GameLoopGameController_Script : MonoBehaviour
         ActivateButtons();
         pausedGamePanel.SetActive(false);
         gameIsPaused = false;
-        UpdateButtonColors();
     }
 
     public void ReturnToMainMenu()
@@ -176,6 +248,16 @@ public class GameLoopGameController_Script : MonoBehaviour
         gameIsWon = false;
         gameIsLost = false;
         ActivateButtons();
+        SceneManager.LoadScene(0);
+    }
+
+    public void ReturnToMainMenuAndReset()
+    {
+        gameIsPaused = false;
+        gameIsWon = false;
+        gameIsLost = false;
+        ActivateButtons();
+        Data.Reset();
         SceneManager.LoadScene(0);
     }
 
@@ -193,7 +275,7 @@ public class GameLoopGameController_Script : MonoBehaviour
         pauseButton.interactable = true;
     }
 
-    private void UpdateButtonColors()
+    private void UpdateButtonColors() //makes buttons appear in correct color when coming back mid game, wont need later, feedback is for my debugging
     {
         ColorBlock walkingButtonColors = walkingButton.colors;
         ColorBlock runningButtonColors = runningButton.colors;
@@ -229,19 +311,161 @@ public class GameLoopGameController_Script : MonoBehaviour
     {
         gameLosePanelGameObject.SetActive(true);
         gameIsPaused = true;
-        Data.Reset();
     }
 
     private void WinGame()
     {
         gameWinPanelGameObject.SetActive(true);
         gameIsPaused = true;
-        Data.Reset();
     }
+
+    private void GetRandomEventOrEnemy()
+    {
+        gameIsPaused = true;
+
+        List<GameObject> possibleEncounters = new List<GameObject>();
+
+        if (!Data.enemyOneMet)
+        {
+            possibleEncounters.Add(enemyOneGameObject);
+        }
+
+        if (!Data.enemyTwoMet)
+        {
+            possibleEncounters.Add(enemyTwoGameObject);
+        }
+
+        if (!Data.eventOneMet)
+        {
+            possibleEncounters.Add(eventOneGameObject);
+        }
+
+        if (!Data.eventTwoMet)
+        {
+            possibleEncounters.Add(eventTwoGameObject);
+        }
+
+        if (possibleEncounters.Count > 0)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, possibleEncounters.Count);
+            GameObject randomEncounter = possibleEncounters[randomIndex];
+            
+            if (randomEncounter.Equals(enemyOneGameObject))
+            {
+                Data.enemyOneMet = true;
+                Data.activeEventOrEnemy = "ENEMY";
+                enemyOneGameObject.SetActive(true);
+            }
+
+            if (randomEncounter.Equals(enemyTwoGameObject))
+            {
+                Data.enemyTwoMet = true;
+                Data.activeEventOrEnemy = "ENEMY";
+                enemyTwoGameObject.SetActive(true);
+            }
+
+            if (randomEncounter.Equals(eventOneGameObject))
+            {
+                Data.eventOneMet = true;
+                Data.activeEventOrEnemy = "EVENT";
+                eventOneGameObject.SetActive(true);
+            }
+
+            if (randomEncounter.Equals(eventTwoGameObject))
+            {
+                Data.eventTwoMet = true;
+                Data.activeEventOrEnemy = "EVENT";
+                eventTwoGameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.Log("NoEncountersLeft");
+        }
+    }
+
+    private void PlayerWinInEnemy()
+    {
+        RecoverHealth(2);
+    }
+
+    private void PlayerWinInEvent()
+    {
+        RecoverHealth(5);
+    }
+    private void PlayerLoseInEnemy()
+    {
+        TakeDamage(5, "PLAYER");
+    }
+
+    private void PlayerLoseInEvent()
+    {
+        TakeDamage(2, "PLAYER");
+    }
+
+    private void TakeDamage(int damage, String bossOrPlayer)
+    {
+        if (bossOrPlayer == "PLAYER")
+        {
+            Data.playerHealth -= damage;
+        }
+
+        if (bossOrPlayer == "BOSS")
+        {
+            bossCurrentHealth -= damage;
+        }
+    }
+
+    private void RecoverHealth(int healthToRecover)
+    {
+        Data.playerHealth += healthToRecover;
+
+        if (Data.playerHealth > maxPlayerHealth)
+        {
+            Data.playerHealth = maxPlayerHealth;
+        }
+    }
+
+    public void LoseViaTemporaryButton()
+    {
+        Data.playerLose = true;
+        DeactivateAllEncounterPanels();
+        gameIsPaused = false;
+    }
+
+    public void WinViaTemporaryButton()
+    {
+        Data.playerWin = true;
+        DeactivateAllEncounterPanels();
+        gameIsPaused = false;
+    }
+
+    private void DeactivateAllEncounterPanels()
+    {
+        enemyOneGameObject.SetActive(false);
+        enemyTwoGameObject.SetActive(false);
+        eventOneGameObject.SetActive(false);
+        eventTwoGameObject.SetActive(false);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     //TO - DO
-    //fiender
-    //random fiende väljare
-    //välja klass innan fiende och visa vilka minigames / klass
-    //Items
-    //Boss
+    //2 fiender som är kopplat till enstaka minigames --> kan ta skada eller förlora item
+    //2 event tvingat specifikt minigame --> kan få item, hälsa eller förlora hälsa eller ingenting händer
+    //random fiende och event väljare i game loopen
+    //Items som påverkar boss fight
+    //Vid förlust mot vanlig fiende kan man förlora item
+    //Boss --> 3 olika spel för bossen
+    //Fiender med olika attribut (vissa gör mer skada andra andra har större chans att sno item ex drake vs goblin)
 }
--- Assets/Scripts/Zakarias/Data.cs
+++ Assets/Scripts/Zakarias/Data.cs
@@ -6,22 +6,45 @@ public class Data : MonoBehaviour
 {
     public static float playerProgress;
     public static float playerHealth;
-    
-    public static bool walking;
-    public static bool running;
 
-    public static int activeEnemyPointer;
+    public static string activeEventOrEnemy; //Skickas in som 'EVENT' eller 'ENEMY'
 
-    public static void Load() {
+    public static bool walking, running;
+    public static bool hasItemSword, hasItemSheild;
+    public static bool enemyOneMet, enemyTwoMet;
+    public static bool eventOneMet, eventTwoMet;
+    public static bool playerWin, playerLose;
+    public static bool encounterOneMet, encounterTwoMet;
+    public static bool encounterThreeMet, encounterFourMet;
+
+    public static int currentActiveMinigame;
+
+    public static void Load() //man ska kunna komma tillbaka till gameloop scenen och ett minigame eller event om man lämnade mitt i det
+    {
 
     }
 
-    public static void Reset() {
+    public static void Reset() //återställer all sparad data
+    {
         playerProgress = 0;
         playerHealth = GameLoopGameController_Script.maxPlayerHealth;
+        walking = false;
+        running = false;
+        enemyOneMet = false;
+        enemyTwoMet = false;
+        eventOneMet = false;
+        eventTwoMet = false;
+        encounterOneMet = false;
+        encounterTwoMet = false;
+        encounterThreeMet = false;
+        encounterFourMet = false;
+        hasItemSheild = false;
+        hasItemSword = false;
+        activeEventOrEnemy = "";
     }
 
-    public static void SaveToFile() {
+    public static void SaveToFile() //för att skriva till .json
+    {
 
     }
 }
