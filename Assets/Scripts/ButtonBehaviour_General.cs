85bd726ffbbbdef6b85f013775139c128556f640
Kajsaherge <kajsaherge@gmail.com>
2024-05-13 23:13:05 +0200

    Animerat scenerna
    Har nu fadeat in och ut från mina minispel

--- Assets/Scripts/ButtonBehaviour_General.cs
+++ Assets/Scripts/ButtonBehaviour_General.cs
@@ -1,13 +1,23 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonBehaviour : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void LoadScene()
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadLevel());
     }
-    
+
+    private IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene(1);
+    }
 }
