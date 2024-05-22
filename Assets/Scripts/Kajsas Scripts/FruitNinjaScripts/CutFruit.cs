5b87a39a29b513941b797a4f110251a41e7b5859
Kajsaherge <kajsaherge@gmail.com>
2024-05-22 14:34:40 +0200

    Ändrat sprite + partikeleffekt
    Ändrade emerald då någon sa att det var copy av minecraft, har bytt musik flr alla spel till egengjord musik och la till en partikeleffekt för när gems går sönder

--- Assets/Scripts/Kajsas Scripts/FruitNinjaScripts/CutFruit.cs
+++ Assets/Scripts/Kajsas Scripts/FruitNinjaScripts/CutFruit.cs
@@ -1,17 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static ShowScore_fruitNinja;
 
 public class CutFruit : MonoBehaviour
 {
+    [SerializeField] private GameObject genericParticle;
+
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Cut")
         {
+            Vector2 contactPoint = collision.contacts[0].point;
+            GameObject particleInstance = Instantiate(genericParticle, contactPoint, Quaternion.identity);
+
+            Destroy(particleInstance, 0.5f);
+
             Destroy(this.gameObject);
             GameObject scoreText = GameObject.Find("ShowScore");
             scoreText.GetComponent<ShowScore_fruitNinja>().IncrementScore(1);
         }
     }
+
 }
